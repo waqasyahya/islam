2c7fb544ca66a8e6c558257f0cf8a14e0e2ebce5
Waqas Yahya <waqasyahya143s@gmail.com>
2025-02-09 19:10:08 +0500

    change

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -1,22 +1,24 @@
 <?php
 
 namespace App\Console;
-use App\Models\Eventday;
+
 use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Facades\Log;
+
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule)
-{
-    $schedule->call(function () {
-        Eventday::where('end_time', '<', now())->delete();
-    })->daily();
-}
+    {
+        $schedule->call(function () {
+            Log::info('2 minutes ho gaye hain, i.');
+        })->everyTwoMinutes();
+    }
 
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
